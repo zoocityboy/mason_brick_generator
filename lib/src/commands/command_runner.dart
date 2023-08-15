172005e5d15666222adca15f62c95de8736aa2a8
zoocityboy <zoocityboy@gmail.com>
2023-08-15 10:59:56 +0200

    feat: init templates

--- lib/src/commands/command_runner.dart
+++ lib/src/commands/command_runner.dart
@@ -3,11 +3,9 @@ import 'package:args/command_runner.dart';
 import '../localisation.dart';
 import '../utils/config_mixin.dart';
 import '../utils/console_mixin.dart';
-import 'discover.dart';
 import 'generate.dart';
 import 'init.dart';
 import 'list.dart';
-import 'validate.dart';
 
 /// Command Runner for Mason Brick Generator
 class MasonTplCommandRunner extends CommandRunner<void> with ConsoleMixin, ConfigMixin {
@@ -17,8 +15,8 @@ class MasonTplCommandRunner extends CommandRunner<void> with ConsoleMixin, Confi
     addCommand(ListCommand());
     addCommand(GenerateCommand());
 
-    addCommand(DiscoverCommand());
-    addCommand(ValidateCommand());
+    // addCommand(DiscoverCommand());
+    // addCommand(ValidateCommand());
   }
   @override
   String get usageFooter => Localisation.cmdFooter;
